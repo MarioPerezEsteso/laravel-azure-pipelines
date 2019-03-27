55c546163e1ae5d8df741bc39001e6f661ea2fa2
Mario Pérez Esteso <mario@geekytheory.com>
2019-03-27 17:12:03 +0100

    first commit

---
+++ tests/Unit/ExampleCodeceptionTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class ExampleCodeceptionTest extends \Codeception\Test\Unit
+{
+    /**
+     * @throws Exception
+     */
+    public function testThatTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
