058dacc7cf9073ac47f4eb0cec5afbddfda99ff3
marioperezesteso <mperez@iseazy.com>
2019-03-28 12:33:46 +0000

    test(codeception): add dummy tests

---
+++ tests/acceptance/ExampleTestCest.php
@@ -0,0 +1,14 @@
+<?php 
+
+class ExampleTestCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->assertTrue(true);
+    }
+}
---
+++ tests/functional/ExampleTestCest.php
@@ -0,0 +1,14 @@
+<?php 
+
+class ExampleTestCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+        $I->assertTrue(true);
+    }
+}
